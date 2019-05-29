1cddcad9c65efdd1c239134cc6e77460435fe3b6
artekm <44265668+artekm@users.noreply.github.com>
2019-05-29 15:25:02 +0200

    Spacja

--- src/main/java/com/itacademy/enumy/App.java
+++ src/main/java/com/itacademy/enumy/App.java
@@ -9,6 +9,7 @@ public class App {
 		Scanner in = new Scanner(System.in);
 		return in.nextLine();
 	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("Working days: "+DayOfWeek.getWorkingDays());

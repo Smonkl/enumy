975bfa60fc2649c0d1fac3853dbc32783ce77ec4
artekm <44265668+artekm@users.noreply.github.com>
2019-05-29 19:45:50 +0200

    nnnnnn

--- src/main/java/com/itacademy/enumy/DayOfWeek.java
+++ src/main/java/com/itacademy/enumy/DayOfWeek.java
@@ -6,34 +6,39 @@ import org.apache.commons.lang3.*;
 //javaDoc generator, extra  javadoc options
 //-encoding UTF-8 -charset UTF-8 -docencoding UTF-8
 
-
 public enum DayOfWeek {
-	MONDAY("Poniedziałek", false), 
-	TUESDAY("Wtorek", false), 
-	WEDNESDAY("Środa", false), 
+	MONDAY("Poniedziałek", false),
+	TUESDAY("Wtorek", false),
+	WEDNESDAY("Środa", false),
 	THURSDAY("Czwartek", false),
-	FRIDAY("Piątek", false), 
-	SATURDAY("Sobota", true), 
+	FRIDAY("Piątek", false),
+	SATURDAY("Sobota", true),
 	SUNDAY("Niedziela", true);
 
-    /**
-     * <p>Returns polish name of given enum instance.</p>
-     *
-     * <p>Polish letters (ĄĆĘŁŃÓŚŻąćęłńóśż) are used in names.</p>
-     *
-     * @return Polish name for enum
-     * @since 1.0
-     */
+	/**
+	 * <p>
+	 * Returns polish name of given enum instance.
+	 * </p>
+	 *
+	 * <p>
+	 * Polish letters (ĄĆĘŁŃÓŚŻąćęłńóśż) are used in names.
+	 * </p>
+	 *
+	 * @return Polish name for enum
+	 * @since 1.0
+	 */
 	public String getPolishName() {
 		return polishName;
 	}
 
-    /**
-     * <p>Checks if given enum instance is weekend day.</p>
-     *
-     * @return {@code true} if the day is weekend day
-     * @since 1.0
-     */
+	/**
+	 * <p>
+	 * Checks if given enum instance is weekend day.
+	 * </p>
+	 *
+	 * @return {@code true} if the day is weekend day
+	 * @since 1.0
+	 */
 	public boolean isWeekendDay() {
 		return isWeekendDay;
 	}
@@ -45,41 +50,50 @@ public enum DayOfWeek {
 		this.polishName = polishName;
 		this.isWeekendDay = isWeekendDay;
 	}
-	
-    /**
-     * <p>Provides a list of working days.</p>
-     *
-     * @return Unmodifiable list of working days
-     * @since 1.0
-     */
-	public static Collection<DayOfWeek> getWorkingDays(){
-		return List.of(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY);
+
+	/**
+	 * <p>
+	 * Provides a list of working days.
+	 * </p>
+	 *
+	 * @return Unmodifiable list of working days
+	 * @since 1.0
+	 */
+	public static Collection<DayOfWeek> getWorkingDays() {
+		return List.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY);
 	}
 
-    /**
-     * <p>Provides a list of weekend days.</p>
-     *
-     * @return Unmodifiable list of weekend days
-     * @since 1.0
-     */
-	public static Collection<DayOfWeek> getWeekendDays(){
-		return List.of(SATURDAY,SUNDAY);
+	/**
+	 * <p>
+	 * Provides a list of weekend days.
+	 * </p>
+	 *
+	 * @return Unmodifiable list of weekend days
+	 * @since 1.0
+	 */
+	public static Collection<DayOfWeek> getWeekendDays() {
+		return List.of(SATURDAY, SUNDAY);
 	}
 
-    /**
-     * <p>Returns enum instance by polish name.</p>
-     *
-     * <p>Polish letters (ĄĆĘŁŃÓŚŻąćęłńóśż) are used in names.</p>
-     *
-     * @param polishName the String of polish name
-     * @return enum instance if the name matches, otherwise {@code null}
-     * @since 1.0
-     */
+	/**
+	 * <p>
+	 * Returns enum instance by polish name.
+	 * </p>
+	 *
+	 * <p>
+	 * Polish letters (ĄĆĘŁŃÓŚŻąćęłńóśż) are used in names.
+	 * </p>
+	 *
+	 * @param polishName the String of polish name
+	 * @return enum instance if the name matches, otherwise {@code null}
+	 * @since 1.0
+	 */
 	public static DayOfWeek getByPolishName(String polishName) {
-		if (StringUtils.isBlank(polishName)) return null;
+		if (StringUtils.isBlank(polishName))
+			return null;
 
 		DayOfWeek[] days = DayOfWeek.values();
-		for (DayOfWeek day:days) {
+		for (DayOfWeek day : days) {
 			if (day.polishName.equalsIgnoreCase(polishName))
 				return day;
 		}
@@ -87,4 +101,3 @@ public enum DayOfWeek {
 	}
 
 }
-
--- src/main/java/com/itacademy/enumy/Enumy.java
+++ src/main/java/com/itacademy/enumy/Enumy.java
@@ -3,23 +3,22 @@ package com.itacademy.enumy;
 import java.util.Scanner;
 
 public class Enumy {
-	
+
 	private static String askForName() {
 		System.out.println("Enter polish day's name: ");
 		Scanner in = new Scanner(System.in);
 		return in.nextLine();
 	}
-	
 
 	public static void main(String[] args) {
-		System.out.println("Working days: "+DayOfWeek.getWorkingDays());
-		System.out.println("Weekend days: "+DayOfWeek.getWeekendDays());
+		System.out.println("Working days: " + DayOfWeek.getWorkingDays());
+		System.out.println("Weekend days: " + DayOfWeek.getWeekendDays());
 
 		DayOfWeek[] days = DayOfWeek.values();
-		for (DayOfWeek day:days) {
+		for (DayOfWeek day : days) {
 			System.out.println(day.getPolishName());
 		}
-		
+
 		do {
 			try {
 				String polishName = askForName();

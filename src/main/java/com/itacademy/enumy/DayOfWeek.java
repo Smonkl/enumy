c79673242867dc0069677cd0d917fa22dbadd5a8
artekm <44265668+artekm@users.noreply.github.com>
2019-05-29 15:55:50 +0200

    I have added javadoc? comments to methods.

--- src/main/java/com/itacademy/enumy/DayOfWeek.java
+++ src/main/java/com/itacademy/enumy/DayOfWeek.java
@@ -12,10 +12,24 @@ public enum DayOfWeek {
 	SATURDAY("Sobota", true), 
 	SUNDAY("Niedziela", true);
 
+    /**
+     * <p>Returns polish name of given enum instance.</p>
+     *
+     * <p>Polish letters (ĄĆĘŁŃÓŚŻąćęłńóśż) are used in names.</p>
+     *
+     * @return Polish name for enum
+     * @since 1.0
+     */
 	public String getPolishName() {
 		return polishName;
 	}
 
+    /**
+     * <p>Checks if given enum instance is weekend day.</p>
+     *
+     * @return {@code true} if the day is weekend day
+     * @since 1.0
+     */
 	public boolean isWeekendDay() {
 		return isWeekendDay;
 	}
@@ -28,14 +42,35 @@ public enum DayOfWeek {
 		this.isWeekendDay = isWeekendDay;
 	}
 	
+    /**
+     * <p>Provides a list of working days.</p>
+     *
+     * @return Unmodifiable list of working days
+     * @since 1.0
+     */
 	public static Collection<DayOfWeek> getWorkingDays(){
 		return List.of(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY);
 	}
 
+    /**
+     * <p>Provides a list of weekend days.</p>
+     *
+     * @return Unmodifiable list of weekend days
+     * @since 1.0
+     */
 	public static Collection<DayOfWeek> getWeekendDays(){
 		return List.of(SATURDAY,SUNDAY);
 	}
 
+    /**
+     * <p>Returns enum instance by polish name.</p>
+     *
+     * <p>Polish letters (ĄĆĘŁŃÓŚŻąćęłńóśż) are used in names.</p>
+     *
+     * @param polishName the String of polish name
+     * @return enum instance if the name matches, otherwise {@code null}
+     * @since 1.0
+     */
 	public static DayOfWeek getByPolishName(String polishName) {
 		if (StringUtils.isBlank(polishName)) return null;
 
